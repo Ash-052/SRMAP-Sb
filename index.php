57a8fde4f407b7042f53f67297e65501d1b68113
dhanushvemulapalli <dhanush_vemulapalli@srmap.edu.in>
2024-04-07 18:24:07 +0530

    Changed everything to .php from .html

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Login</h1>
+        <div class="login-box">
+            <a href="student login form-1.php"><button class="student-login">Student Login</button></a>
+
+            
+            <a href="loginoption.html"><button class="employee-login">Employee Login</button></a>
+        </div>
+    </div>
+</body>
+</html>
